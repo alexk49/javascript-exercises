fee7031b6f9218731d5484c03f01bc734d741221
alexk49 <alexcode9@mailbox.org>
2024-06-09 09:54:14 +0100

    first commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    let counter = 0
+    while (counter < num) {
+        newString = newString + string;
+        counter += 1;
+    }
+    return newString;
 };
 
 // Do not edit below this line
