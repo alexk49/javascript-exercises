da96edf7c38cb38b3702807515b4c059de7ba125
alexk49 <alexcode9@mailbox.org>
2024-06-09 10:14:06 +0100

    first commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let characters = Array.from(string);
+    let reversed = "";
 
+    for (let i = 1; i < string.length + 1; i++) {
+        reverseIndex = (string.length - i);
+        reversed = reversed + characters[reverseIndex];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
