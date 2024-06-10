8113945335142b61c6976dd2b78d559dd003b493
alexk49 <alexcode9@mailbox.org>
2024-06-10 08:14:42 +0100

    first commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let output = 0;
 
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return ("ERROR")
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return ("ERROR")
+    }
+
+    let smallerNum = 0;
+    let largerNum = 0;
+
+    if (num1 < num2) {
+        smallerNum = num1;
+        largerNum = num2;
+    }
+    else {
+        smallerNum = num2;
+        largerNum = num1;
+    }
+
+    for (let counter = smallerNum; counter <= largerNum; counter += 1) {
+        output = output + counter;
+    }
+    return output;
 };
 
 // Do not edit below this line
