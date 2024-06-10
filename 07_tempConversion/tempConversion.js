7f6b613d9dcb4df8fade020ddf034f6486dd964f
alexk49 <alexcode9@mailbox.org>
2024-06-10 08:50:05 +0100

    first commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    // x °F ≘ (x − 32) × 5/9 °C 
+    return roundNumToOneDecimalPlace((fahrenheit - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    // x °C ≘ (x × 9/5 + 32) °F 
+    return roundNumToOneDecimalPlace((celsius * (9 / 5) + 32));
 };
+
+const roundNumToOneDecimalPlace = function(number) {
+    return Math.round(number * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
