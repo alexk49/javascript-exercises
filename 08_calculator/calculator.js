fb19258da0ecea56dee7c8e344b460c4fa9fce2a
alexk49 <alexcode9@mailbox.org>
2024-07-08 08:47:53 +0100

    first commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
-};
+const add = function (num, num1) {
+  return num + num1
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (num, num1) {
+  return num - num1
+}
 
-const sum = function() {
-	
-};
+const sum = function (numbers) {
+  return numbers.reduce((total, num) => {
+    return total + num
+  }, 0)
+}
 
-const multiply = function() {
+const multiply = function (numbers) {
+  return numbers.reduce((total, num) => {
+    return total * num
+  })
+}
 
-};
+const power = function (num, num1) {
+  return Math.pow(num, num1)
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function (num) {
+  if (num === 0) {
+    return 1
+  } else {
+    let total = 1
+    for (let i = 1; i <= num; i++) {
+      total = total * i
+    }
+    return total
+  }
+}
 
 // Do not edit below this line
 module.exports = {
@@ -30,4 +42,4 @@ module.exports = {
   multiply,
   power,
   factorial
-};
+}
