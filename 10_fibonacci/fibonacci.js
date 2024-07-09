8058a15025260fb5950746d11f387197c1e00b81
alexk49 <alexcode9@mailbox.org>
2024-07-09 12:17:45 +0100

    first commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+  // 1, 1, 2, 3, 5, 8
+  //  Fn = F{n-1}+F{n-2}} for n > 1.
+  num = Number(num)
+  if (num === 0) {
+    return 0
+  } else if (num === 1 || num === 2) {
+    return 1
+  } else if (num < 0) {
+    return 'OOPS'
+  } else {
+    let f = 2
+    f = fibonacci(num - 1) + fibonacci(num - 2)
+    return f
+  }
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
