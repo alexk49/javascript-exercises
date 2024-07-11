84811f8331774c387eff7371b8286708f0a577ec
alexk49 <alexcode9@mailbox.org>
2024-07-11 08:12:32 +0100

    first commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+  const oldestPerson = people.reduce((oldest, person) => {
+    if (Object.keys(oldest).length === 0) {
+      return person
+    }
+    const age = person.yearOfDeath - person.yearOfBirth
+    if (age > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+      return person
+    } else {
+      return oldest
+    }
+  },
+  {})
+  return oldestPerson
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
