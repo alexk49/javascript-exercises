c48e8f699dd50130c606e80017a71fd1e5603fbd
alexk49 <alexcode9@mailbox.org>
2024-07-11 08:21:57 +0100

    added function to work with living people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,18 @@
+const getAge = function (yearOfBirth, yearOfDeath) {
+  if (!yearOfDeath) {
+    yearOfDeath = new Date().getFullYear()
+  }
+  return yearOfDeath - yearOfBirth
+}
+
 const findTheOldest = function (people) {
   const oldestPerson = people.reduce((oldest, person) => {
     if (Object.keys(oldest).length === 0) {
       return person
     }
-    const age = person.yearOfDeath - person.yearOfBirth
-    if (age > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath)
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    if (personAge > oldestAge) {
       return person
     } else {
       return oldest
